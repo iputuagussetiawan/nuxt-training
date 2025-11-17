1129aaf81e86dc5a0c3dd4da703af6fcc04ef4e2
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-11-17 15:55:00 +0800

    update pavicon nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            link: [
+                { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+                { rel: 'shortcut icon', href: '/favicon.ico' }
+            ]
+        }
+    },
     modules: [
         '@nuxtjs/google-fonts',
         '@nuxt/image',
